77e5dd376a36cc1b39ce332c8fee7fa02d616085
Jesse Stricker <978183+jessestricker@users.noreply.github.com>
2026-03-25 23:18:18 +0100

    feat: add ESLint component (#13)

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,7 @@
 import { GithubCredentials } from "projen/lib/github";
 import { NodePackageManager, NpmAccess } from "projen/lib/javascript";
+import { Eslint, Prettier } from "./src/javascript";
 import { TypeScriptProject } from "projen/lib/typescript";
-import { Prettier } from "./src/javascript/prettier";
 import { GeneratePackageExports } from "./src/typescript/generate-package-exports";
 
 const project = new TypeScriptProject({
@@ -17,7 +17,7 @@ const project = new TypeScriptProject({
   // node package & dependencies
   packageManager: NodePackageManager.PNPM,
   peerDeps: ["constructs@^10", "projen@>=0.98.1 <1.0.0"],
-  deps: ["@dprint/formatter", "@dprint/typescript"],
+  deps: ["@dprint/formatter", "@dprint/typescript", "type-fest"],
 
   // typescript
   tsconfig: { compilerOptions: { skipLibCheck: true } },
@@ -28,7 +28,7 @@ const project = new TypeScriptProject({
   sampleCode: false,
 
   // formatting & linting
-  eslintOptions: { dirs: ["src"], prettier: true },
+  eslint: false,
 
   // git & github
   gitignore: [".idea/"],
@@ -47,7 +47,17 @@ const project = new TypeScriptProject({
   releaseToNpm: true,
 });
 
-new Prettier(project, { yaml: true });
+new Eslint(project, {
+  files: [project.srcdir, project.testdir, ".projenrc.ts"],
+  typescriptEslint: true,
+  typescriptEslintStrict: true,
+  typescriptEslintStylistic: true,
+});
+
+new Prettier(project, {
+  yaml: true,
+});
+
 new GeneratePackageExports(project);
 
 project.synth();
--- src/javascript/index.ts
+++ src/javascript/index.ts
@@ -1,2 +1,4 @@
+export * from "./eslint";
+export * from "./eslint-config";
 export * from "./javascript-file";
 export * from "./prettier";
